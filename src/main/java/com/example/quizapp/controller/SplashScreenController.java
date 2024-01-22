02dc43790f920f67497667744ede65b1729fb43b
ELOUAIZI Ayoub <ayoubelouaizi@gmail.com>
2024-01-22 20:09:44 +0100

    splashscreen

---
+++ src/main/java/com/example/quizapp/controller/SplashScreenController.java
@@ -0,0 +1,12 @@
+package com.example.quizapp.controller;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+
+public class SplashScreenController {
+
+    @FXML
+    private Label welcomeLabel;
+
+    // You can add any additional logic or initialization here
+}
